e57a53b25b855c73b4ed4f8f03aca41057c1ea0b
marieduetto123 <marie.dare@duettoresearch.com>
2026-05-09 22:53:55 +0100

    Partial chip: use filled lock icon same as Closed

--- travelcore-rm-hub.js
+++ travelcore-rm-hub.js
@@ -1754,7 +1754,7 @@ function renderCalendar() {
           <span class="cell-hdr-spacer">${_showEye ? eyeSvg : ''}</span>
         </div>
         ${isLocked && !isCompact ? '<span class="cell-closed-label">Closed' + _lockFilled + '</span>' : ''}
-        ${hasCalCl && !isCompact ? '<span class="cell-partial-close-label" style="cursor:pointer" onmouseenter="calShowEventTip(event,\'' + m.month + '-' + d + '\')" onmouseleave="calHideEventTip()">Partial' + _lockOutlined + '</span>' : ''}
+        ${hasCalCl && !isCompact ? '<span class="cell-partial-close-label" style="cursor:pointer" onmouseenter="calShowEventTip(event,\'' + m.month + '-' + d + '\')" onmouseleave="calHideEventTip()">Partial' + _lockFilled + '</span>' : ''}
         ${!isCompact ? `<div class="cell-content">${metricRows}</div>` : ''}
         ${!isCompact && hasCalEvents ? '<span class="cell-event-ico" onmouseenter="calShowEventTip(event,\''+m.month+'-'+d+'\')" onmouseleave="calHideEventTip()"><span class="material-icons" style="font-size:16px;color:#006461">today</span></span>' : ''}
       </div>`;
